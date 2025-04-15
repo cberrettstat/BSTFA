f52108ab304dbee5a3be0569eca10083faccc916
Candace Berrett <cberrett@fang.byu.edu>
2025-04-15 10:49:29 -0600

    updates to plot.location

--- R/posterior_inference.R
+++ R/posterior_inference.R
@@ -254,8 +254,8 @@ plot.location = function(out, location, new_x=NULL,
          ylim=ylim,
          lty=2)
     if (uncertainty) {
-      lines(y=ymat.preds.lb[xlims,i], x=out$dates[xlims], col='green', lty=2)
-      lines(ymat.preds.ub[xlims,i], x=out$dates[xlims], col='green', lty=2)
+      polygon(x=c(out$dates[xlims], rev(out$dates[xlims])), y=c(ymat.preds.lb[xlims,i], rev(ymat.preds.ub[xlims,i])), border=NA, col=rgb(.1, .1, .1, .2))
+      #lines(ymat.preds.ub[xlims,i], x=out$dates[xlims], col='green', lty=2)
     }
     if (truth & is.null(dim(location))) lines(y=out$ymat[xlims,location[i]],
                                               x=out$dates[xlims], col='black', lty=1)
